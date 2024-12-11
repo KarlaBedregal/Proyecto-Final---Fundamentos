2bf21158a699bda4b66cbfc516da6b2d39129ab6
JoseMorocco <jmoroccosa@unsa.edu.pe>
2024-12-10 23:34:08 -0500

    Se agrega la clase Jugador donde se guardaran las fichas de este

--- JuegoLudo/src/juegoludo/Jugador.java
+++ JuegoLudo/src/juegoludo/Jugador.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package juegoludo;
+import java.awt.Graphics2D;
 
-/**
- *
- * @author Asus
- */
 public class Jugador {
-    
+    int altura, ancho, estado, fichas; // VARIABLES QUE DEFINEN EL JUGADOR
+    Ficha[] fichasActivas = new Ficha[4]; // ARREGLO DE FICHAS DEL JUGADOR
+
+    // CONSTRUCTOR DE LA CLASE JUGADOR
+    public Jugador(int altura, int ancho) {
+        estado = -1; // INICIALIZA EL ESTADO DEL JUGADOR A -1 (FUERA DEL JUEGO)
+        fichas = 0; // INICIALIZA EL CONTEO DE FICHAS EN 0
+        for (int i = 0; i < 4; i++) {
+            fichasActivas[i] = new Ficha(altura, ancho); // INICIALIZA CADA FICHA CON LAS DIMENSIONES DADAS
+        }
+    }
+
+    // MÉTODO PARA DIBUJAR EL JUGADOR 
+    public void dibujar(Graphics2D graficos) {
+        // Código auxiliar de método generado automáticamente
+    }
 }
